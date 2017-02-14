07ccd894c13e14539112ed67f104ac58289823ba
djain2 <djain2@ncsu.edu>
2017-02-13 23:56:19 -0500

    Code for blog post about how to launch activity only the first time

---
+++ SignUpActvitySample/app/src/main/java/com/divyajain/signupactvitysample/MainActivity.java
@@ -0,0 +1,30 @@
+package com.divyajain.signupactvitysample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
+
+        if (isFirstRun) {
+            //show start activity
+
+            startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            Toast.makeText(MainActivity.this, "First Run", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+
+        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
+                .putBoolean("isFirstRun", false).commit();
+    }
+}
---
+++ SignUpActvitySample/app/src/main/java/com/divyajain/signupactvitysample/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.divyajain.signupactvitysample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Divya Jain on 2/13/2017.
+ */
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+    }
+}
